2691ed7e4f4695190453c3536f4931c98750f2b3
lalan9 <150842280+lalan9@users.noreply.github.com>
2024-01-17 16:15:26 +0800

    Update ipv46.sh

--- ipv46.sh
+++ ipv46.sh
@@ -4,9 +4,8 @@
 ipv4=$(curl -s https://ipinfo.io/ip)
 
 # 获取IPv6地址
-ipv6=$(curl -6 ifconfig.co)
+ipv6=$(ip -6 addr show scope global | grep inet6 | awk '{print $2}' | cut -d '/' -f 1)
 
 # 显示结果
 echo "IPv4地址: $ipv4"
 echo "IPv6地址: $ipv6"
-
