2a9a6a02b40d6ca56a30778ad543b49416e27e0f
lalan9 <150842280+lalan9@users.noreply.github.com>
2024-02-15 00:27:40 +0800

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -1,34 +1,112 @@
 #!/bin/bash
 
-# Function to check if IPv6 is enabled
-ipv6_enabled() {
-    if [ "$(sysctl -n net.ipv6.conf.all.disable_ipv6)" -eq 0 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
+export LC_ALL=C
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 
-# Function to get the IPv6 gateway address
-get_ipv6_gateway() {
-    ip -6 route show default | awk '/via/ {print $3}'
-}
-
-# Function to set the default IPv6 route
-set_ipv6_route() {
-    gateway="$1"
-    ip -6 route add default via "$gateway" dev "$(ip -6 route show default | awk '/via/ {print $5}')" metric 1024
-}
-
-# Main script
-if ipv6_enabled; then
-    ipv6_gateway=$(get_ipv6_gateway)
-    if [ -n "$ipv6_gateway" ]; then
-        echo "IPv6 is enabled, using gateway: $ipv6_gateway"
-        set_ipv6_route "$ipv6_gateway"
-    else
-        echo "Failed to obtain IPv6 gateway address."
-    fi
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    sudoCmd="sudo"
 else
-    echo "IPv6 is not enabled."
+    sudoCmd=""
 fi
+
+# fonts color
+red(){
+    echo -e "\033[31m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m$1\033[0m"
+}
+blue(){
+    echo -e "\033[36m$1\033[0m"
+}
+
+Green_font_prefix="\033[32m" 
+Red_font_prefix="\033[31m" 
+Green_background_prefix="\033[42;37m" 
+Red_background_prefix="\033[41;37m" 
+Font_color_suffix="\033[0m"
+
+osInfo=""
+osRelease=""
+osReleaseVersion=""
+osReleaseVersionNo=""
+osReleaseVersionCodeName="CodeName"
+osSystemPackage=""
+osSystemMdPath=""
+osSystemShell="bash"
+
+osKernelVersionFull=$(uname -r)
+osKernelVersionBackup=$(uname -r | awk -F "-" '{print $1}')
+osKernelVersionShort=$(uname -r | cut -d- -f1 | awk -F "." '{print $1"."$2}')
+osKernelBBRStatus=""
+systemBBRRunningStatus="no"
+systemBBRRunningStatusText=""
+
+# Check system version
+getLinuxOSVersion(){
+    if [[ -s /etc/redhat-release ]]; then
+        osReleaseVersion=$(grep -oE '[0-9.]+' /etc/redhat-release)
+    else
+        osReleaseVersion=$(grep -oE '[0-9.]+' /etc/issue)
+    fi
+
+    if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        . /etc/os-release
+        osInfo=$NAME
+        osReleaseVersionNo=$VERSION_ID
+
+        if [ -n $VERSION_CODENAME ]; then
+            osReleaseVersionCodeName=$VERSION_CODENAME
+        fi
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        osInfo=$(lsb_release -si)
+        osReleaseVersionNo=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        . /etc/lsb-release
+        osInfo=$DISTRIB_ID
+        
+        osReleaseVersionNo=$DISTRIB_RELEASE
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        osInfo=Debian
+        osReleaseVersion=$(cat /etc/debian_version)
+        osReleaseVersionNo=$(sed 's/\..*//' /etc/debian_version)
+    elif [ -f /etc/redhat-release ]; then
+        osReleaseVersion=$(grep -oE '[0-9.]+' /etc/redhat-release)
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        osInfo=$(uname -s)
+        osReleaseVersionNo=$(uname -r)
+    fi
+}
+
+# Check system release code name
+function getLinuxOSRelease(){
+    if [[ -f /etc/redhat-release ]]; then
+        osRelease="centos"
+        osSystemPackage="yum"
+        osSystemMdPath="/usr/lib/systemd/system/"
+        osReleaseVersionCodeName=""
+    elif cat /etc/issue | grep -Eqi "debian|raspbian"; then
+        osRelease="debian"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="buster"
+    elif cat /etc/issue | grep -Eqi "ubuntu"; then
+        osRelease="ubuntu"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="bionic"
+    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+        osRelease="centos"
+        osSystemPackage="yum"
+        osSystemMdPath="/usr/lib/systemd/system/"
+        osReleaseVersionCodeName=""
+    elif cat /proc/version | grep -Eqi "
