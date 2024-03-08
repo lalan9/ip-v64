2dd7b046dde2224f185c98db554d77061fd9a6f1
lalan9 <150842280+lalan9@users.noreply.github.com>
2024-03-08 18:08:34 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -3,12 +3,10 @@
 # Author : MXiDev
 # Website : https://mxidev.com
 
-if [ $1 != 'restore' ];then
-    DNS1=$1
-    DNS2=$2
-fi
+# 函数定义开始
 
-function Get_OSName(){
+# 获取操作系统名称
+get_os_name() {
     if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
         DISTRO='CentOS'
     elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
@@ -16,125 +14,69 @@ function Get_OSName(){
     elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
         DISTRO='Ubuntu'
     else
-        DISTRO='unknow'
+        DISTRO='unknown'
     fi
-    if [ $DISTRO != 'unknow' ]; then
-        echo -e '检测到您的系统为: '$DISTRO''
+
+    if [ "$DISTRO" != 'unknown' ]; then
+        echo -e "检测到您的系统为: $DISTRO"
     else
-        echo -e '不支持的操作系统，请更换为 CentOS / Debian / Ubuntu 后重试。'
-        exit 1;
+        echo -e "不支持的操作系统，请更换为 CentOS / Debian / Ubuntu 后重试。"
+        exit 1
     fi
 }
-function get_char(){
-    SAVEDSTTY=`stty -g`
+
+# 捕获用户输入字符
+get_char() {
+    SAVEDSTTY=$(stty -g)
     stty -echo
     stty cbreak
     dd if=/dev/tty bs=1 count=1 2> /dev/null
     stty -raw
     stty echo
-    stty $SAVEDSTTY
+    stty "$SAVEDSTTY"
 }
-function Welcome(){
+
+# 欢迎信息和DNS确认
+welcome() {
     echo -e '正在检测您的操作系统...'
-    Get_OSName
-    echo -e '您确定要使用下面的DNS地址吗？'
-    echo -e '主DNS: '$DNS1''
-    if [ "$DNS2" != '' ]; then
-        echo -e '备DNS: '$DNS2''
+    get_os_name
+    echo -e "您确定要使用下面的DNS地址吗？\n主DNS: $1"
+    [ -n "$2" ] && echo -e "备DNS: $2"
+    echo -e "\n请按任意键继续，如有配置错误请使用 Ctrl+C 退出。"
+    get_char
+}
+
+# 修改DNS配置
+change_dns() {
+    echo -e '\n正在备份当前DNS配置文件...'
+    cp /etc/resolv.conf /etc/resolv.conf.backup
+    echo -e '备份完成，正在修改DNS配置文件...'
+    {
+        echo "nameserver $1"
+        [ -n "$2" ] && echo "nameserver $2"
+    } > /etc/resolv.conf
+    echo -e 'DNS配置文件修改完成。\n'
+}
+
+# 恢复DNS配置
+restore_dns() {
+    echo -e '正在恢复默认DNS配置文件...'
+    mv /etc/resolv.conf.backup /etc/resolv.conf
+    echo -e 'DNS配置文件恢复完成。\n'
+}
+
+# 主逻辑开始
+
+if [ "$1" != 'restore' ]; then
+    # 检查是否提供了至少一个DNS地址
+    if [ -z "$1" ]; then
+        echo "用法错误：需要指定至少一个DNS地址或使用 'restore' 选项。"
+        exit 1
     fi
-    echo
-    echo -e '请按任意键继续，如有配置错误请使用 Ctrl+C 退出。'
-    char=`get_char`
-}
-function ChangeDNS(){
-    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
-        echo
-        echo -e '正在备份当前DNS配置文件...'
-        cp /etc/resolv.conf /etc/resolv.conf.backup
-        echo
-        echo -e '备份完成，正在修改DNS配置文件...'
-        if [ `cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'` == 7 ]; then
-            sed -i '/\[main\]/a dns=none' /etc/NetworkManager/NetworkManager.conf
-            systemctl restart NetworkManager.service
-        fi
-        echo -e 'nameserver '$DNS1'' > /etc/resolv.conf
-        if [ "$DNS2" != '' ]; then
-            echo -e 'nameserver '$DNS2'' >> /etc/resolv.conf
-        fi
-        echo
-        echo -e 'DNS配置文件修改完成。'
-    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
-        echo
-        echo -e '正在备份当前DNS配置文件...'
-        cp /etc/resolv.conf /etc/resolv.conf.backup
-        echo
-        echo -e '备份完成，正在修改DNS配置文件...'
-        echo -e 'nameserver '$DNS1'' > /etc/resolv.conf
-        if [ "$DNS2" != '' ]; then
-            echo -e 'nameserver '$DNS2'' >> /etc/resolv.conf
-        fi
-        echo
-        echo -e 'DNS配置文件修改完成。'
-    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
-        echo
-        echo -e '正在修改DNS配置文件...'
-        if [ `cat /etc/issue|awk '{print $2}'|awk -F'.' '{print $1}'` -le 17 ]; then
-            echo -e 'nameserver '$DNS1'' > /etc/resolvconf/resolv.conf.d/base
-            if [ "$DNS2" != '' ]; then
-                echo -e 'nameserver '$DNS2'' >> /etc/resolvconf/resolv.conf.d/base
-            fi
-            resolvconf -u
-        else
-            echo -e 'nameserver '$DNS1'' >> /etc/systemd/resolved.conf
-            if [ "$DNS2" != '' ]; then
-                echo -e 'nameserver '$DNS2'' >> /etc/systemd/resolved.conf
-            fi
-            systemctl restart systemd-resolved.service
-        fi
-        echo
-        echo -e 'DNS配置文件修改完成。'
-    fi
-    echo
-    echo -e '感谢您的使用, 如果您想恢复备份，请在执行脚本文件时使用参数 restore 。'
-}
-function RestoreDNS(){
-    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
-        echo -e '正在恢复默认DNS配置文件...'
-        rm -rf /etc/resolv.conf
-        mv /etc/resolv.conf.backup /etc/resolv.conf
-        if [ `cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'` == 7 ]; then
-            sed -i 's/dns=none//g' /etc/NetworkManager/NetworkManager.conf
-            systemctl restart NetworkManager.service
-        fi
-        echo
-        echo -e 'DNS配置文件恢复完成。'
-    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
-        echo -e '正在恢复默认DNS配置文件...'
-        rm -rf /etc/resolv.conf
-        mv /etc/resolv.conf.backup /etc/resolv.conf
-        echo
-        echo -e 'DNS配置文件恢复完成。'
-    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
-        echo -e '正在恢复默认DNS配置文件...'
-        if [ `cat /etc/issue|awk '{print $2}'|awk -F'.' '{print $1}'` -le 17 ]; then
-            echo -e '' > /etc/resolvconf/resolv.conf.d/base
-            resolvconf -u
-        else
-            sed -i '/nameserver/d' /etc/systemd/resolved.conf
-            systemctl restart systemd-resolved.service
-        fi
-        echo
-        echo -e 'DNS配置文件恢复完成。'
-    fi
-}
-function addDNS(){
-    Welcome
-    ChangeDNS
-}
-if [ $1 != 'restore' ];then
-    addDNS
-elif [ $1 == 'restore' ];then
-    RestoreDNS
+    welcome "$1" "$2"
+    change_dns "$1" "$2"
 else
-    echo '用法错误！'
+    restore_dns
 fi
+
+echo -e '感谢您的使用, 如果您想恢复备份，请在执行脚本文件时使用参数 restore 。'
