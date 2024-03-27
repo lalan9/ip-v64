ad394666eb07dcdb0a53b28177091e31319ea7b0
lalan9 <150842280+lalan9@users.noreply.github.com>
2024-03-28 02:09:53 +0800

    Update download1.sh

--- download1.sh
+++ download1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 循环下载文件500次
-for ((i=999; i<=9999999; i++))
+for ((i=1; i<=500; i++))
 do
-    wget https://maxadmin.k888k.top/theme/wordpress-bob/js/chunk-vendors.dac02230.js -P /path/to/save
+    wget https://maxadmin.k888k.top/theme/wordpress-bob/js/chunk-vendors.dac02230.js
 done
