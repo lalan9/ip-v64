36f49cb0906b6f9415a44abff37b8224758bf2d3
lalan9 <150842280+lalan9@users.noreply.github.com>
2024-03-28 02:26:03 +0800

    Update and rename download1.sh to download8.sh

--- download1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 循环下载文件500次，覆盖下载
-for ((i=50; i<=50000; i++))
-do
-    wget -N https://apimax.k888k.top/theme/wordpress-bob/js/chunk-vendors.dac02230.js -O chunk-vendors.dac02230.js
-done
