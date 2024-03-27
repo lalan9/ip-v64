2fd442c17c9a60b8026041672d35700fbf03a893
lalan9 <150842280+lalan9@users.noreply.github.com>
2024-03-28 02:29:33 +0800

    Update and rename download8.sh to download4.sh

--- download8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 循环下载文件500次，覆盖下载
-for ((i=50; i<=50000; i++))
-do
-    wget -N https://119.82.25.58/_next/static/chunks/framework-c16fc4c01675a4d8.js -O framework-c16fc4c01675a4d8.js
-done
