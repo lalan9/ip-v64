3d65cbeb3ea5af07105d7e6baf53a0c34324c1d2
lalan9 <150842280+lalan9@users.noreply.github.com>
2024-03-28 01:41:50 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,5 +3,5 @@
 # 循环下载文件500次
 for ((i=999; i<=9999999; i++))
 do
-    wget https://hmkj3.com/theme/pink/assets/i18n/en-US.js -P /path/to/save
+    wget https://hmkj3.com/theme/pink/assets/umi.css -P /path/to/save
 done
