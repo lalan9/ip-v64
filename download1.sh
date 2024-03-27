c1c8292c45a85b998df0a2f27643d7b2f62db72d
lalan9 <150842280+lalan9@users.noreply.github.com>
2024-03-28 02:21:49 +0800

    Update download1.sh

--- download1.sh
+++ download1.sh
@@ -3,5 +3,5 @@
 # 循环下载文件500次，覆盖下载
 for ((i=50; i<=50000; i++))
 do
-    wget -N https://maxadmin.k888k.top/theme/wordpress-bob/js/chunk-vendors.dac02230.js -O chunk-vendors.dac02230.js
+    wget -N https://apimax.k888k.top/theme/wordpress-bob/js/chunk-vendors.dac02230.js -O chunk-vendors.dac02230.js
 done
